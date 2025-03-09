3c78856bf435a36b3bbb6945578f2f5209029a28
Nanami Iwata <g2220510@is.ocha.ac.jp>
2025-03-09 15:52:12 +0900

    add:pagesのindex.tsを追加

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+export { default as HomePage } from './HomePage';
+export { default as LoginPage } from './LoginPage';
+export { default as RegisterPage } from './RegisterPage';
+export { default as TodoListPage } from './TodoListPage';
+export { default as TodoEditPage } from './TodoEditPage';
