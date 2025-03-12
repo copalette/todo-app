494982e99f5642cb38dc74ec9303ebfab8a6a43a
Nanami Iwata <g2220510@is.ocha.ac.jp>
2025-03-12 19:15:48 +0900

    ホームページのUIを実装

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,24 @@
 import { Link } from "react-router-dom";
 
-function HomePage(){
+function HomePage() {
     return (
-        <div>
-  
+        <div className="home-page">
+            <div className="home-container">
+                <h1 className="home-title">Todoアプリへようこそ</h1>
+                <p className="home-description">
+                    このアプリでは、タスクを簡単に管理することができます。
+                    始めるには、ログインまたは新規登録してください。
+                </p>
+                
+                <div className="home-buttons">
+                    <Link to="/login" className="home-button login-button">
+                        ログイン
+                    </Link>
+                    <Link to="/register" className="home-button register-button">
+                        新規登録
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
