65ff9ed3f6c747ec4c4a6f3762a91721b55c2977
Nanami Iwata <g2220510@is.ocha.ac.jp>
2025-03-12 18:12:53 +0900

    supabaseのセットアップ

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import './App.css'
+import './styles/App.css'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import { HomePage,LoginPage,TodoListPage,TodoEditPage,RegisterPage } from './pages'
+import { createClient } from '@supabase/supabase-js'
+
+// Supabaseクライアントの初期化
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 function App() {
 
