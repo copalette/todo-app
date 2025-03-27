1924262612449e1bd9dabdc6d39dfd29c9ec2eb5
Nanami Iwata <nanami.iwata510@gmail.com>
2025-03-27 14:51:33 +0900

    未使用のインポートを削除

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { Todo } from '../types';
-import { todoService } from '../services/todoService';
 
 interface TodoItemProps {
   todo: Todo;
